e3d0ecb139e00bc9e22853e0df688ed82247ccc9
akarapong <97168241+0xAkarapong@users.noreply.github.com>
2025-01-23 16:53:04 +0700

    Rename and update function to plot_segments
    Renamed `parse_segments` to `plot_segments` to better reflect its functionality. Increased the number of generated segments for plotting from 10,000 to 100,000 for improved visualization testing.

--- Homework day 7.py
+++ Homework day 7.py
@@ -6,7 +6,7 @@ def generate_random_segments(num_segments):
     segments = np.random.randint(0, 11, (num_segments, 4))
     return segments
 
-def parse_segments(segments):
+def plot_segments(segments):
     x = segments[:, [0, 2]].T
     y = segments[:, [1, 3]].T
     plt.plot(x, y)
@@ -16,9 +16,9 @@ plt.ylabel('Y-axis')
 plt.title('Segments Plot')
 plt.grid(True)
 
+segments = generate_random_segments(100000)
 t1 = time.time()
-segments = generate_random_segments(10000)
-parse_segments(segments)
+plot_segments(segments)
 t2 = time.time()
 plt.show()
 print(f"Time taken: {t2-t1} seconds")
