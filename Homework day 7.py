2c694847a38b4fd436ceba828b36869fbe3835c5
akarapong <97168241+0xAkarapong@users.noreply.github.com>
2025-01-23 17:00:45 +0700

    Optimize segment filtering using NumPy implementation.
    Replaced the loop-based filtering method with a NumPy-based approach for improved performance. Increased the number of generated segments from 100,000 to 1,000,000 to leverage the enhanced efficiency. This modification significantly reduces computation time for large datasets.

--- Homework day 7.py
+++ Homework day 7.py
@@ -16,6 +16,22 @@ def filter_segments_loop(segments, region):
             filtered_segments.append(segment)
     return filtered_segments
 
+def filter_segments_numpy(segments, region):
+    xmin, ymin, xmax, ymax = region
+    segments_np = np.array(segments)
+    x1 = segments_np[:, 0]
+    y1 = segments_np[:, 1]
+    x2 = segments_np[:, 2]
+    y2 = segments_np[:, 3]
+
+    mask = (
+            (x1 >= xmin) & (x1 <= xmax) &
+            (y1 >= ymin) & (y1 <= ymax) &
+            (x2 >= xmin) & (x2 <= xmax) &
+            (y2 >= ymin) & (y2 <= ymax)
+    )
+    filtered_segments_np = segments_np[mask]
+    return filtered_segments_np.tolist()
 
 def plot_segments(segments):
     plt.xlabel('X-axis')
@@ -29,10 +45,10 @@ def plot_segments(segments):
     y_coords = np.array(segments)[:, [1, 3]].T
     plt.plot(x_coords, y_coords, linewidth=0.5)
 
-segments = generate_random_segments(100000)
+segments = generate_random_segments(1000000)
 region_of_interest = [0, 1, 4, 4]
 t1 = time.time()
-filtered_segments = filter_segments_loop(segments, region_of_interest)
+filtered_segments = filter_segments_numpy(segments, region_of_interest)
 plot_segments(filtered_segments)
 t2 = time.time()
 plt.show()
